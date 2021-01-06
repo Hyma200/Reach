40eb33e945f8ba6341ecef0e561640458f4a2b7f
aka-andi <andrea.motas@gmail.com>
2021-01-06 00:57:44 -0500

    add navigation connections (home, search, log, saved)

--- app/src/main/java/com/example/virtualvolunteer/Saved.java
+++ app/src/main/java/com/example/virtualvolunteer/Saved.java
@@ -1,22 +1,18 @@
 package com.example.virtualvolunteer;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity {
-
+public class Saved extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_saved);
 
         BottomNavigationView navView = findViewById(R.id.Bottom_navigation_icon);
         Navigation.enableNavigationClick(this, navView);
@@ -25,7 +21,4 @@ public class Home extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
     }
-
-
-
 }
