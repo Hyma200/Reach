9387af12de86c9e6259101de7acd0e69efec43d4
aka-andi <andrea.motas@gmail.com>
2021-01-06 14:07:12 -0500

    include navigation toolbar in profile

--- app/src/main/java/com/example/virtualvolunteer/Profile.java
+++ app/src/main/java/com/example/virtualvolunteer/Profile.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 //import android.widget.TextView;
 //import android.widget.ImageView;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
     //private TextView name;
@@ -15,5 +19,12 @@ public class Profile extends AppCompatActivity {
         //image = (ImageView) findViewById(R.id.image);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        BottomNavigationView navView = findViewById(R.id.Bottom_navigation_icon);
+        Navigation.enableNavigationClick(this, navView);
+
+        Menu menu = navView.getMenu();
+        MenuItem menuItem = menu.getItem(4);
+        menuItem.setChecked(true);
     }
 }
