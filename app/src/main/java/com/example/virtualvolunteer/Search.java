21fc47401e8b2bc8d5f9834842e764f97311df3a
hyma2 <Hyma200@gmail.com>
2021-01-07 14:41:12 -0500

    Searching in Database

--- app/src/main/java/com/example/virtualvolunteer/Search.java
+++ app/src/main/java/com/example/virtualvolunteer/Search.java
@@ -1,18 +1,44 @@
 package com.example.virtualvolunteer;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class Search extends AppCompatActivity {
+
+    private EditText name;
+    private ImageView image;
+    private TextView debug;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference users = database.getReference("Users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        name = (EditText) findViewById(R.id.user);
+        image = (ImageView) findViewById(R.id.search_activity);
+        debug = (TextView) findViewById(R.id.debug);
 
         BottomNavigationView navView = findViewById(R.id.Bottom_navigation_icon);
         Navigation.enableNavigationClick(this, navView);
@@ -20,5 +46,33 @@ public class Search extends AppCompatActivity {
         Menu menu = navView.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
+
+        image.setOnClickListener(v -> users.orderByChild("name").equalTo(name.getText().toString()).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                debug.append(snapshot.toString());
+                debug.append(snapshot.child("email").getValue().toString());
+                debug.append(snapshot.child("name").getValue().toString());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                debug.append(error.toString());
+            }
+        }));
     }
 }
