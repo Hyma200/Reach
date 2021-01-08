cfea29ba1630fcf0dfb25f1bb27d9f0f93f94013
aka-andi <andrea.motas@gmail.com>
2021-01-08 17:51:18 -0500

    attempt to store post data in firebase (help pls lol)

--- app/src/main/java/com/example/virtualvolunteer/PostCreate.java
+++ app/src/main/java/com/example/virtualvolunteer/PostCreate.java
@@ -1,9 +1,11 @@
 package com.example.virtualvolunteer;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,17 +14,27 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.squareup.picasso.Picasso;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
 public class PostCreate extends AppCompatActivity {
 
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    DatabaseReference myRef = database.getReference("Users").child(user.getEmail().replace('.', '_'));
     private EditText description;
     private Button postBtn;
     private Button postImagePicker;
     private ImageView postImage;
     private Uri imageUri;
     private final int PICK_IMAGE_REQUEST = 123;
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference("uploads");
+    private StorageTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +51,14 @@ public class PostCreate extends AppCompatActivity {
         Navigation.enableNavigationClick(this, navView);
 
         postBtn.setOnClickListener(v -> {
-            String fDescription = description.getText().toString();
-            if (fDescription.isEmpty()) {
+            if (description.getText().toString().isEmpty()) {
                 Toast toast = Toast.makeText(PostCreate.this, "Description cannot be empty",
                         Toast.LENGTH_SHORT);
                 toast.show();
                 return;
             }
-            Toast toast = Toast.makeText(PostCreate.this, "Successfully Made New Post",
+            //storePost(description.getText().toString());
+            Toast toast = Toast.makeText(PostCreate.this, "Successfully made new post",
                     Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
@@ -60,6 +72,32 @@ public class PostCreate extends AppCompatActivity {
                 startActivityForResult(imagePickerIntent, PICK_IMAGE_REQUEST);
             }
         });
+
+    }
+
+    public void storePost(String description) {
+        StorageReference photoRef = storageRef.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+        photoRef.putFile(imageUri).continueWithTask(photoUploadTask -> {
+            photoRef.getDownloadUrl();
+            return null;
+        }).continueWithTask(downloadURLTask -> {
+            Post post = new Post(user.getDisplayName(), description, downloadURLTask.getResult().toString(), System.currentTimeMillis());
+            // TODO:  add to firebase database post collection
+            return null;
+        }).addOnCompleteListener(postCreationTask -> {
+            if (!postCreationTask.isSuccessful()) {
+                Toast toast = Toast.makeText(PostCreate.this, "Failed to create new post",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        });
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
     public void reload() {
